da92c6797f9cf8004658aeef7b2c947c7547b928
andre <andrepb636@gmail.com>
2024-03-23 21:25:55 -0300

    Add page user registration

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,12 @@
 <template>
-<header></header>
+<header>
+
+    <nav>
+        <router-link to="/" class="menu-header-option">Home</router-link>
+        <router-link to="/correcao" class="menu-header-option">Testes</router-link>
+        <router-link to="/cadastro" class="menu-header-option">Cadastro de paciente</router-link>
+    </nav>
+</header>
 </template>
 
 <script>
@@ -9,12 +16,22 @@ export default {
 </script>
 
 <style scoped>
+.menu-header-option {
+    color: var(--br);
+    text-decoration: none;
+}
+
+nav {
+    display: flex;
+    justify-content: space-between;
+    width: 60%;
+}
 header {
     display: flex;
     background-color: var(--vd4);
     height: 50px;
     color: var(--br);
     align-items: center;
-    justify-content: center;
+    justify-content:center;
 }
 </style>
